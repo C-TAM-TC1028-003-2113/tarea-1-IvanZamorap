bcbf6c2a9cc648207f628d13eab6602a066e4119
IvanZamorap <89656651+IvanZamorap@users.noreply.github.com>
2021-08-29 21:17:01 -0500

    Update exercise.py
    Listo

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -1,7 +1,15 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
+    import math
+    palabras = int(input("Dame el número de palabras: "))
+    cuartilla = int(math.ceil(palabras / 475))
+    
+    precio = (cuartilla * 60)
+    preciofinal = precio - (precio * 0.10)
+    
+    print("El costro de la publicación es:" , preciofinal)
 
 
 if __name__ == '__main__':
     main()
+
