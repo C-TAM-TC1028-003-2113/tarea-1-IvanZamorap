a2d08970f0fbff75221a03175f3ef83c9e1a1b4e
IvanZamorap <89656651+IvanZamorap@users.noreply.github.com>
2021-08-29 21:06:37 -0500

    Update exercise.py
    Listo

--- assignments/10Videojuego/src/exercise.py
+++ assignments/10Videojuego/src/exercise.py
@@ -1,6 +1,11 @@
 def main():
-    # escribe tu código abajo de esta línea
-    pass
+      # escribe tu código abajo de esta línea
+    nuevos = int(input('Dame la cantidad de juegos nuevos: '))
+    usados = int(input('Dame la cantidad de juegos usados: '))
+
+    total = (nuevos * 1000) + (usados * 350)
+    print('El total de la compra es:', total)
+
 
 
 if __name__ == '__main__':
